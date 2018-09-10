3fe8202ab99393ae482fe72b4c0e93d12478736e
nadispose <nadispose@gmail.com>
2018-09-10 03:45:43 -0400

    factss .java

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -3,7 +3,25 @@ package math.problems;
 /**
  * Created by mrahman on 04/02/18.
  */
-public class Factorial {
+public class Factorial
+{
+    static int iter(int n)
+    {
+        int num = 1;
+        for(int i = n; i > 1; i--)
+        {
+            num *= i;
+        }
+        return num;
+    }
+
+    static int recur(int n)
+    {
+        if (n<=1)
+            return 1;
+        else
+            return(n * recur(n-1));
+    }
 
     public static void main(String[] args) {
         /*
@@ -12,5 +30,8 @@ public class Factorial {
          *
          */
 
+        System.out.println(iter(5));
+        int num = recur(5);
+        System.out.println("recursion: " + num);
     }
 }
